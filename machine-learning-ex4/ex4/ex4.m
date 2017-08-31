98c67b05a68b3b8e075181d9f713e4d6df10a982
Yee Jie Tang <yeejietang@gmail.com>
2017-08-31 16:17:46 +0800

    ex4 completion

--- machine-learning-ex4/ex4/ex4.m
+++ machine-learning-ex4/ex4/ex4.m
@@ -74,8 +74,7 @@ fprintf('\nFeedforward Using Neural Network ...\n')
 % Weight regularization parameter (we set this to 0 here).
 lambda = 0;
 
-J = nnCostFunction(nn_params, input_layer_size, hidden_layer_size, ...
-                   num_labels, X, y, lambda);
+J = nnCostFunction(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lambda);
 
 fprintf(['Cost at parameters (loaded from ex4weights): %f '...
          '\n(this value should be about 0.287629)\n'], J);
@@ -183,27 +182,22 @@ fprintf('\nTraining Neural Network... \n')
 
 %  After you have completed the assignment, change the MaxIter to a larger
 %  value to see how more training helps.
-options = optimset('MaxIter', 50);
+options = optimset('MaxIter', 400);
 
 %  You should also try different values of lambda
 lambda = 1;
 
 % Create "short hand" for the cost function to be minimized
-costFunction = @(p) nnCostFunction(p, ...
-                                   input_layer_size, ...
-                                   hidden_layer_size, ...
-                                   num_labels, X, y, lambda);
+costFunction = @(p) nnCostFunction(p, input_layer_size, hidden_layer_size, num_labels, X, y, lambda);
 
 % Now, costFunction is a function that takes in only one argument (the
 % neural network parameters)
 [nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
 
 % Obtain Theta1 and Theta2 back from nn_params
-Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
-                 hidden_layer_size, (input_layer_size + 1));
+Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), hidden_layer_size, (input_layer_size + 1));
 
-Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
-                 num_labels, (hidden_layer_size + 1));
+Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), num_labels, (hidden_layer_size + 1));
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -1,8 +1,4 @@
-function [J grad] = nnCostFunction(nn_params, ...
-                                   input_layer_size, ...
-                                   hidden_layer_size, ...
-                                   num_labels, ...
-                                   X, y, lambda)
+function [J grad] = nnCostFunction(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lambda)
 %NNCOSTFUNCTION Implements the neural network cost function for a two layer
 %neural network which performs classification
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
@@ -16,11 +12,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 
 % Reshape nn_params back into the parameters Theta1 and Theta2, the weight matrices
 % for our 2 layer neural network
-Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
-                 hidden_layer_size, (input_layer_size + 1));
-
-Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
-                 num_labels, (hidden_layer_size + 1));
+Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), hidden_layer_size, (input_layer_size + 1));
+Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), num_labels, (hidden_layer_size + 1));
 
 % Setup some useful variables
 m = size(X, 1);
@@ -38,7 +31,22 @@ Theta2_grad = zeros(size(Theta2));
 %         variable J. After implementing Part 1, you can verify that your
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
-%
+
+% forward prop
+a_1 = [ones(m, 1) X];
+z_2 = a_1 * Theta1';
+a_2 = sigmoid(z_2);
+a_2 = [ones(m, 1) a_2];
+z_3 = a_2 * Theta2';
+a_3 = sigmoid(z_3);
+
+% set up matrix for y
+all_combos = eye(num_labels);
+y_matrix = all_combos(y,:);
+
+% sum over all output labels, and sum over all training examples
+J = sum(sum((-y_matrix .* log(a_3)) - ((1-y_matrix) .* log(1-a_3))))/m;
+
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
 %         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
@@ -47,38 +55,46 @@ Theta2_grad = zeros(size(Theta2));
 %
 %         Note: The vector y passed into the function is a vector of labels
 %               containing values from 1..K. You need to map this vector into a 
-%               binary vector of 1's and 0's to be used with the neural network
+%               binary vector of 1's and 0's to be used withTTh the neural network
 %               cost function.
 %
 %         Hint: We recommend implementing backpropagation using a for-loop
 %               over the training examples if you are implementing it for the 
 %               first time.
-%
+
+% d3
+d_3 = a_3 - y_matrix;
+
+% d2
+d_2 = (d_3 * Theta2(:,2:end)) .* sigmoidGradient(z_2);
+
+% Deltas
+Delta_1 = d_2' * a_1;
+Delta_2 = d_3' * a_2;
+
+% Theta Gradients
+Theta1_grad = Delta_1/m;
+Theta2_grad = Delta_2/m;
+
 % Part 3: Implement regularization with the cost function and gradients.
 %
 %         Hint: You can implement this around the code for
 %               backpropagation. That is, you can compute the gradients for
 %               the regularization separately and then add them to Theta1_grad
 %               and Theta2_grad from Part 2.
-%
 
+% Regularize Cost function
+reg_cost = ( sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2)) ) * lambda / 2 / m;
+J = J + reg_cost;
 
+% Regularize Theta Gradients
+reg_theta1_grad = lambda*Theta1/m;
+reg_theta1_grad(:,1) = 0;
+Theta1_grad = Theta1_grad + reg_theta1_grad;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+reg_theta2_grad = lambda*Theta2/m;
+reg_theta2_grad(:,1) = 0;
+Theta2_grad = Theta2_grad + reg_theta2_grad;
 
 % -------------------------------------------------------------
 
